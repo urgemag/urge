8e3919fe29929755a893b87ed3415e1e30997085
Revisto <walt.shabani@gmail.com>
2021-02-20 01:00:43 +0330

    add setting.py

---
+++ db/flask/setting.py
@@ -0,0 +1,52 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED*** RECAPTCHA setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED*** end of RECAPTCHA setting ***REMOVED***
+
+***REMOVED*** MONGODB setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED*** end of MONGODB setting ***REMOVED***
+
+***REMOVED*** ZARINPAL setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED*** end of ZARINPAL setting ***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
