283dda3a2d65a02006f2152699eb3d91b8121d3a
Revisto <walt.shabani@gmail.com>
2021-02-28 13:12:09 +0330

    move flask out of db/

---
+++ flask/setting.py
@@ -0,0 +1,51 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+load_dotenv(dotenv_path="../../.env")
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+
+
+***REMOVED*** RECAPTCHA setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED*** end of RECAPTCHA setting ***REMOVED***
+
+***REMOVED*** MONGODB setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED*** end of MONGODB setting ***REMOVED***
+
+***REMOVED*** ZARINPAL setting ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED*** end of ZARINPAL setting ***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
