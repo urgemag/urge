f97a4f8f9077647ff0debbf7daef58d2ae6ab0ea
Revisto <walt.shabani@gmail.com>
2021-03-02 12:13:57 +0330

    merge use of json files in topics survey, add random background color

--- flask/views/basic.py
+++ flask/views/basic.py
@@ -40,7 +40,8 @@ def index():
         number_of_courses = PageDetails().number_of_courses(),
         top_courses = PageDetails().top_3_expensive_courses(),
         quotes = PageDetails().random_quotes(),
-        random_blog_post = PageDetails().get_random_blog_post()
+        random_blog_post = PageDetails().get_random_blog_post(),
+        survey = PageDetails().get_survey_json_data()
         )
     
 
